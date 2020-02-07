fa10bf0ec5c8162d84d222435e999c48620a56ed
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-02-07 15:49:01 -0600

    scaffoulding and navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+function Navbar() {
+    return (
+<nav className = "navbar navbar-dark bg-dark" >
+    <span class = "navbar-text">
+    Employee Tracker 
+    </span>   
+    </nav>
+    );
+}
+
+export default Navbar;
