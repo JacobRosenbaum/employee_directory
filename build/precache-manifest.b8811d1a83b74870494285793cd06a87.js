eb8e93fa4cebd854c45e6358cae642140c0b412c
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-02-14 13:47:17 -0600

    adding build folder

---
+++ build/precache-manifest.b8811d1a83b74870494285793cd06a87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344264f1a99daaebcb55c96f92cdd228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "652c7aa1d308e447b5c5",
+    "url": "/static/css/main.0b3cca39.chunk.css"
+  },
+  {
+    "revision": "3e36099da7c271fb1300",
+    "url": "/static/js/2.d7d99a60.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d7d99a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652c7aa1d308e447b5c5",
+    "url": "/static/js/main.6c415abd.chunk.js"
+  },
+  {
+    "revision": "07a6439f1d7a70d4de98",
+    "url": "/static/js/runtime-main.d50cb326.js"
+  },
+  {
+    "revision": "78ee9c8a6a9f322010a59e932bc83920",
+    "url": "/static/media/vintage-wallpaper.78ee9c8a.png"
+  }
+]);
